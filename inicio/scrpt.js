60ea5a17fc86ec309a9f9f4bd721887aa5dbb528
Valeria <corvale762@gmail.com>
2026-05-04 17:05:21 -0500

    primer commit

---
+++ inicio/scrpt.js
@@ -0,0 +1,61 @@
+let imagenes = [
+  {
+    url: "imagen1.webp",
+    nombre: "Futbol",
+    descripcion: "Aquí encontraras toda la información sobre futbol",
+  },
+  {
+    url: "imagen2.webp",
+    nombre: "Baloncesto",
+    descripcion: "Aquí encontraras toda la información sobre baloncesto",
+  },
+  {
+    url: "imagen3.webp",
+    nombre: "Volleyball",
+    descripcion: "Aquí encontraras toda la información sobre volleyball",
+  },
+];
+
+let atras = document.getElementById("atras");
+let adelante = document.getElementById("adelante");
+let imagen = document.getElementById("img");
+let puntos = document.getElementById("puntos");
+let texto = document.getElementById("texto");
+let actual = 0;
+
+atras.addEventListener("click", function () {
+  actual -= 1;
+
+  if (actual == -1) {
+    actual = imagenes.length - 1;
+  }
+  imagen.innerHTML = `<img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img> `;
+  texto.innerHTML = ` 
+  <h3>${imagenes[actual].nombre}</h3>
+  <p>${imagenes[actual].descripcion}</p>
+  `;
+  posicionCarrusel();
+});
+adelante.addEventListener("click", function () {
+  actual += 1;
+
+  if (actual == imagenes.length) {
+    actual = 0;
+  }
+  imagen.innerHTML = `<img class="img" src="${imagenes[actual].url}" alt="logo pagina" loading="lazy"></img> `;
+  texto.innerHTML = ` 
+  <h3>${imagenes[actual].nombre}</h3>
+  <p>${imagenes[actual].descripcion}</p>
+  `;
+  posicionCarrusel();
+});
+function posicionCarrusel() {
+  puntos.innerHTML = "";
+  for (var i = 0; i < imagenes.length; i++) {
+    if (i == actual) {
+      puntos.innerHTML += '<p class="bold">.</p>';
+    } else {
+      puntos.innerHTML += "<p>.</p>";
+    }
+  }
+}
