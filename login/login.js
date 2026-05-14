e10f02d5d926cce3fbbbe3ddd6da7d4620bd0f26
Isabella <9422iva@gmail.com>
2026-05-14 18:27:49 -0500

    Agregué carpeta de imágenes y footer

---
+++ login/login.js
@@ -0,0 +1,11 @@
+const monster = document.getElementById("monster");
+const inputUsuario = document.getElementById("input-usuario");
+const inputClave = document.getElementById("input-clave");
+const body = document.querySelector("body");
+
+// body.addEventListener("mousemove", () => {
+//   console.log("moviste el mouse");
+// });
+
+console.log(window.innerHeight);
+console.log(window.innerWidth);
