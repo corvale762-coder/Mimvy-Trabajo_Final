08b876ae0fd1be40169989fe00e64ae7749b9f29
Isabella <9422iva@gmail.com>
2026-05-14 22:14:00 -0500

    Agregué cambios al login

--- login/login.js
+++ login/login.js
@@ -2,10 +2,21 @@ const monster = document.getElementById("monster");
 const inputUsuario = document.getElementById("input-usuario");
 const inputClave = document.getElementById("input-clave");
 const body = document.querySelector("body");
+const altoMitad = window.innerHeight / 2;
+const anchoMitad = window.innerWidth / 2;
+let seguirPunteroMouse = true;
 
-// body.addEventListener("mousemove", () => {
-//   console.log("moviste el mouse");
-// });
-
-console.log(window.innerHeight);
-console.log(window.innerWidth);
+body.addEventListener("mousemove", (m) => {
+  if (seguirPunteroMouse) {
+    if (m.clientX < anchoMitad && m.clientY < altoMitad) {
+      monster.src = "imagenes-login/inactivo/2.png";
+    } else if (m.clientX < anchoMitad && m.clientY > altoMitad) {
+      monster.src = "imagenes-login/inactivo/3.png";
+    } else if (m.clientX > anchoMitad && m.clientY < altoMitad) {
+      monster.src = "imagenes-login/inactivo/5.png";
+    } else {
+      monster.src = "imagenes-login/inactivo/4.png";
+    }
+  }
+});
+inputUsuario;
